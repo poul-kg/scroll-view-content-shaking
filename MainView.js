2201a6a0b626964fac771196f7ab0de41a3ac37b
Pavel Kostenko <poul.kg@gmail.com>
2017-12-14 13:31:17 +0600

    first commit

---
+++ MainView.js
@@ -0,0 +1,52 @@
+var Observable = require('FuseJS/Observable');
+var messages = Observable();
+var dummyData = [
+    { text: 'message 1' },
+    { text: 'message 2' },
+    { text: 'message 3' },
+    { text: 'message 4' },
+    { text: 'message 5' },
+    { text: 'message 6' }
+];
+messages.addAll(dummyData);
+
+var chatState = 2;
+
+function goUp() {
+    console.log('goUp');
+    if (chatState < 2) {
+        setChatState(++chatState);
+    }
+}
+
+function goDown() {
+    console.log('goDown');
+    if (chatState > 0) {
+        setChatState(--chatState);
+    }
+}
+
+function setChatState(stateIndex) {
+    switch (stateIndex) {
+        case 2:
+           messagesPanelState.goto(Expanded);
+           console.log('Expanded');
+           break;
+       case 1:
+           messagesPanelState.goto(Half);
+           console.log('Half');
+           break;
+       case 0:
+           messagesPanelState.goto(Collapsed);
+           console.log('Collapsed');
+           break;
+       default:
+           console.log('Undefined state');
+    }
+}
+
+module.exports = {
+    messages: messages,
+    goUp: goUp,
+    goDown: goDown
+};
